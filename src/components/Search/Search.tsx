bb9f140e561b08bf83114bb3ab6fd77b457d1696
Hopelezz <72772558+Hopelezz@users.noreply.github.com>
2022-07-25 14:15:11 -0500

    fixed toLowerCase & includes from erroring, but I still don't get search results

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -9,27 +9,27 @@ interface Props {
 
 export default function Search(props: Props) {
   const [query, setQuery] = createSignal("");
-  console.log(props.posts.frontmatter.title);
-  const lowercased = createMemo(() => props.posts.map(({title, url}) => 
+
+  const lowCaseQuery = createMemo(() => props.posts.map(({title, url}) => 
       ({
-        title: title.toLowerCase(),
+        title: title?.toLowerCase(),
         url
       })
     )
   );
   
-  const filtered = createMemo(() => lowercased().filter(post => 
-    post.title.includes(query().toLowerCase())));
+  const filteredPost = createMemo(() => lowCaseQuery().filter(post => post.title?.includes(query().toLowerCase())));
   
   return <div>
     <input 
       type='text' 
       placeholder='Search...'
-      onInput={(e) => setQuery(e.target.value)} />
+      onInput={(e) => setQuery(e.target.value)} 
+    />
     <Show when={query()}>
       <div class="results">
-        <For each={filtered()}>
-          {(el, i) => <div>{el.name}</div>}
+        <For each={filteredPost()}>
+          {(el, i) => <div>{el.title}</div>}
         </For>
       </div>
     </Show>      

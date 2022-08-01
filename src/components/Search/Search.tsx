c1804617f2754aea6b47225404c24a90fc06a398
Hopelezz <72772558+Hopelezz@users.noreply.github.com>
2022-08-01 18:49:55 -0500

    add feature resolving issue #6 search feature!

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,15 +1,15 @@
 //@ts-check
-// Language: typescript
-// Path: src\components\Search\Search.tsx
-import { createSignal, createMemo, For, Show } from "solid-js";
+/** @jsxImportSource solid-js */
+import { createSignal, For, Show } from "solid-js";
 
-//Inside the posts we are looking at the frontmatter object for Title, Description, and Tags. We will also need to return the URL
+//Inside the posts we are looking at the frontmatter object for Title, Description, Tags, and URL
 //Note: tags is a string separated by commas.
 interface Props {
   posts: Array<{
       frontmatter: {
         title: string;
         publishDate: string;
+        name: string;
         description: string;
         tags: string;
       }
@@ -17,48 +17,45 @@ interface Props {
   }>;
 }
 
-export default function Search({ posts }: Props) {
-  //an export default function called Search that uses the createSignal and createMemo functions to create a search bar. This will filter the posts by using Title, Description, or Tags. The search bar is a input field that takes in a string and returns a list of posts using the PostPreview component that match the search string.
+export default function Search(props: Props) {
   const [search, setSearch] = createSignal("");
-  const filteredPosts = createMemo(() => {
-    //createMemo is a function that creates a memoized function. This will only run the filter function if the search string changes.
-    const searchString = search.toString().toLowerCase();
-    //filters the posts by title, description, or tags
-    return posts.filter(post => {
-      //turns the tags: string into an array of trimmed, lowercase, and split words
-      const tags = post.frontmatter.tags.split(",");
-      tags.forEach(tag => tag.trim().toLowerCase());
-      console.log(tags);
-      return (        
-        post.frontmatter.title.toLowerCase().includes(searchString) ||
-        post.frontmatter.description.toLowerCase().includes(searchString) ||
-        tags.some(tag => tag.includes(searchString))
+  const [filteredPosts, setFilteredPosts] = createSignal(props.posts);
+  
+  function handleInput(e: any) {
+    const { value } = e.currentTarget;
+    setSearch(value.toLowerCase());
+    
+    const filteredPosts = props.posts.filter(post => {
+      const { title, description, tags } = post.frontmatter;
+      //tags is a string separated by commas. Split it into an array of trimmed &lowercase words
+      return (
+        title?.toLowerCase().includes(value) ||
+        description?.toLowerCase().includes(value) ||
+        tags?.toLowerCase().includes(value)
       );
     });
-  }, [posts, search]); 
+    setFilteredPosts(filteredPosts);
+  }
 
-  //return the filtered results the existing posts on index page
   return (
     <div class="search">
-      <br />
-      <p>
-        Searching for something?
-      </p>
-
+      <br/>
+      <p>Searching for something?</p>
       <input
         type="text"
         placeholder="Search"
+        onInput={handleInput}
         value={search()}
-        onChange={e => setSearch(e.target.value)}
       />
-
       <Show when={search()}>
-          <For each={filteredPosts()}>
-            {post => <a href={post.url}><div> {post.frontmatter.title}
-              </div></a>}
-          </For>
+        <For each={filteredPosts()} fallback={<div>No results found</div>}>
+          {(post, i) => 
+            <div data-index={i() + 1}>
+              <a href={post.url}>{post.frontmatter.title}</a>
+            </div>              
+          }
+        </For>
       </Show>
     </div>
-  );
+  );           
 }
-

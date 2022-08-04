b820283ef11e30cd33a7736b02aba9e91cc04cee
Hopelezz <72772558+Hopelezz@users.noreply.github.com>
2022-08-04 18:09:01 -0500

    corrected some issues. Need to update to latest Astro to get last nights error fix for missing link in astro package

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,12 @@
 import { defineConfig } from 'astro/config'; // import preact from '@astrojs/preact';
 
 import solid from '@astrojs/solid-js';
-import vercel from '@astrojs/vercel/serverless';
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [solid(), mdx()],
-  output: 'server',
-  adapter: vercel(),
   markdown: {
     draft: true,
-  }
+  },
 });

48f605e013ce022f7986a92f0378a8709237d34b
Hopelezz <72772558+Hopelezz@users.noreply.github.com>
2022-08-03 20:04:02 -0500

    error could not parse expression with acorn: Unexpected token

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
-import { defineConfig } from 'astro/config';
-// import preact from '@astrojs/preact';
+import { defineConfig } from 'astro/config'; // import preact from '@astrojs/preact';
+
 import solid from '@astrojs/solid-js';
 import vercel from '@astrojs/vercel/serverless';
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [solid()],
-	output: 'server',
-   	adapter: vercel(),
+  integrations: [solid(), mdx()],
+  output: 'server',
+  adapter: vercel()
 });

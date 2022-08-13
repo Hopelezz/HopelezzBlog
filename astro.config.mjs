548d4b675f0c9714429b496aa2e932d219d68548
Hopelezz <72772558+Hopelezz@users.noreply.github.com>
2022-08-13 10:44:30 -0500

    remove double call for solidjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,15 @@
 import { defineConfig } from 'astro/config';
 import { remarkReadingTime } from './remark-reading-time.mjs';
 
-import solid from '@astrojs/solid-js';
 import mdx from "@astrojs/mdx";
 import image from '@astrojs/image';
-
 import solidJs from "@astrojs/solid-js";
 import vercel from '@astrojs/vercel/serverless';
-// import preact from '@astrojs/preact';
+import preact from '@astrojs/preact';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [solid(), mdx(), image(), solidJs()],
+  integrations: [preact(), mdx(), image(), solidJs()],
   output: 'server',
   adapter: vercel(),
   markdown: {

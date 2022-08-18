aa6c054a3b3076f5de2ed22111e304669ea8af60
Hopelezz <72772558+Hopelezz@users.noreply.github.com>
2022-08-18 18:51:00 -0500

    add changes to comment section

--- src/components/comment/comment.ts
+++ src/components/comment/comment.ts
@@ -1,4 +1,6 @@
-import  * as firebase from 'firebase/app';
+import { initializeApp } from "firebase/app";
+import { getFirestore } from 'firebase/firestore';
+import * as firebase from 'firebase/app';
 import 'firebase/firestore';
 
 interface CommentsConfig {
@@ -6,13 +8,32 @@ interface CommentsConfig {
     projectId: string;
 }
 class Comments {
+
     blogTitle: HTMLHeadingElement;
+    firebaseApp: firebase.app.App;
+    commentsRef: firebase.firestore.CollectionReference;
+    loadComments: HTMLButtonElement
 
-    constructor() {
-        this.blogTitle = document.querySelector('.blog-title') as HTMLHeadingElement;
-
+    constructor(config: CommentsConfig) {
+        this.blogTitle = document.querySelector('#blogTitle') as HTMLHeadingElement;
+        this.loadComments = document.querySelector('#loadComments') as HTMLButtonElement;
         //run animations
         this.animateHeader();
+
+        this.firebaseApp = firebase.initializeApp(config);
+        this.commentsRef = this.firebaseApp
+        .firestore()
+        .collection('articles')
+        .doc('article_one')
+        .collection('comments');
+
+        this.addListeners();
+    }
+
+    addListeners() {
+        this.loadComments.addEventListener('click', event => {
+            this.loadComments.classList.add('hidden');
+        });
     }
 
     animateHeader() {
@@ -20,4 +41,20 @@ class Comments {
     }
 }
 
-new Comments();
+new Comments({
+    authDomain: 'blackskies-3522c.firebaseapp.com',
+    projectId: 'blackskies-3522c'
+});
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDrtOpubezws9rxaZbIMYvBFm0myMeOsaE",
+    authDomain: "blackskies-3522c.firebaseapp.com",
+    projectId: "blackskies-3522c",
+    storageBucket: "blackskies-3522c.appspot.com",
+    messagingSenderId: "893625964436",
+    appId: "1:893625964436:web:ab6e04fa689413ee2d6181",
+    measurementId: "G-DPCQEVVBMF"
+  };
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
